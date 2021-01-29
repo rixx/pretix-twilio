f85c40c7de929af87633f3721648d4874f8b6992
Tobias Kunze <r@rixx.de>
2021-01-29 11:41:23 +0100

    Initial commit

---
+++ pretix_twilio/__init__.py
@@ -0,0 +1,30 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.0"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_twilio"
+    verbose_name = "Send SMS with Twilio"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Send SMS with Twilio")
+        author = "Tobias Kunze"
+        description = gettext_lazy(
+            "Additionally to sending emails with pretix, send SMS to your customers with Twilio! Uses the built-in phone number field."
+        )
+        visible = True
+        version = __version__
+        category = "FEATURE"
+        compatibility = "pretix>=2.7.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_twilio.PluginApp"
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import find_packages, setup
+
+from pretix_twilio import __version__
+
+
+try:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+    ) as f:
+        long_description = f.read()
+except Exception:
+    long_description = ""
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command("compilemessages", verbosity=1)
+        build.run(self)
+
+
+cmdclass = {"build": CustomBuild}
+
+
+setup(
+    name="pretix-twilio",
+    version=__version__,
+    description="Additionally to sending emails with pretix, send SMS to your customers with Twilio! Uses the built-in phone number field.",
+    long_description=long_description,
+    url="https://github.com/rixx/pretix-twilio",
+    author="Tobias Kunze",
+    author_email="r@rixx.de",
+    license="Apache",
+    install_requires=[],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretix.plugin]
+pretix_twilio=pretix_twilio:PretixPluginMeta
+""",
+)
