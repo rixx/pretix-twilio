1a5b7236f7cf57d920b5280acce92354295eb077
Tobias Kunze <r@rixx.de>
2021-02-01 14:57:32 +0100

    Show warnings in settings for missing numbers

--- pretix_twilio/views.py
+++ pretix_twilio/views.py
@@ -11,6 +11,14 @@ class TwilioSettings(EventSettingsViewMixin, EventSettingsFormView):
     form_class = TwilioSettingsForm
     template_name = "pretix_twilio/settings.html"
 
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        result["order_phone_asked"] = self.request.event.settings.order_phone_asked
+        result[
+            "order_phone_required"
+        ] = self.request.event.settings.order_phone_required
+        return result
+
     def get_success_url(self, **kwargs):
         return reverse(
             "plugins:pretix_twilio:settings",
